80240bb1d41fb1044da8ad1dad25424c335cc765
phantomized <boon.atticus@gmail.com>
2025-05-15 09:52:53 -0700

    server3

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.server = "10.30.51.251" #ipconfig in command prompt, home ip: 172.16.0.2, school ip: ipconfig
-        self.port = 5555
+        self.port = 12345
         self.addr = (self.server,self.port)
         self.pos = self.connect()
 
