292939ba0865d6bf76c1d2793b6bb61c9ed38f29
phantomized <boon.atticus@gmail.com>
2025-05-22 11:28:00 -0700

    baby map added

--- network.py
+++ network.py
@@ -1,19 +1,9 @@
 import socket
 
-def get_local_ip():
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            sock.connect(('8.8.8.8', 80)) 
-            return sock.getsockname()[0]
-        except:
-            return '127.0.0.1'
-        finally:
-            sock.close()
-
 class Network:
-    def __init__(self):
+    def __init__(self,server_ip):
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server = f"{get_local_ip()}"
+        self.server = server_ip
         self.port = 12345
         self.addr = (self.server,self.port)
         self.pos = self.connect()
@@ -25,13 +15,8 @@ class Network:
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
-        except:
-            network_ip = input("Enter server IP: ")
-            try:
-                self.addr = (network_ip,self.port)
-                self.pos = self.connect
-            except Exception as e:
-                print(f"Error: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
 
     def send(self,data):
         try:
