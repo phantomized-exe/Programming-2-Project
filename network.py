6137979937e6a8ec7aa8599bd9e4f9dd9baff0b3
phantomized <boon.atticus@gmail.com>
2025-05-22 10:04:04 -0700

    made server connection easier

--- network.py
+++ network.py
@@ -1,10 +1,19 @@
 import socket
 
+def get_local_ip():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            sock.connect(('8.8.8.8', 80)) 
+            return sock.getsockname()[0]
+        except:
+            return '127.0.0.1'
+        finally:
+            sock.close()
 
 class Network:
-    def __init__(self,network_ip):
+    def __init__(self):
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server = network_ip #ipconfig in command prompt
+        self.server = f"{get_local_ip()}"
         self.port = 12345
         self.addr = (self.server,self.port)
         self.pos = self.connect()
@@ -16,8 +25,13 @@ class Network:
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
-        except Exception as e:
-            print(f"Error: {e}")
+        except:
+            network_ip = input("Enter server IP: ")
+            try:
+                self.addr = (network_ip,self.port)
+                self.pos = self.connect
+            except Exception as e:
+                print(f"Error: {e}")
 
     def send(self,data):
         try:
