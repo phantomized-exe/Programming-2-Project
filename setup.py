e8a7412a02c0bd3b741e938d1622c79713ba4905
Phanntomized <boona@wlhs.wlwv.k12.or.us>
2025-05-28 16:58:56 -0700

    setup.py fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="Celeste 2 client side",
     options={"build_exe": {"packages":["pygame"],
-                           "include_files":["Test Sprites"]}},
+                           "include_files":["Test Sprites","server.py"]}},
     executables = executables
 
     )
