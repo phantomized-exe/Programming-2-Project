7c2adbae527314f3d1e5b7fa978989ed512ae20d
phantomized <boon.atticus@gmail.com>
2025-05-28 09:23:54 -0700

    e

--- main.py
+++ main.py
@@ -465,14 +465,14 @@ def check_tile_collision_x():
     check_lava_collision()
     tile = check_tile_collision()
     adjust_bg = False
-    if (left.colliderect(player2) or right.colliderect(player2)) and player.velocity_x == 0 and not feet_rect.collidecrect(player2):# and player2.velocity_x != 0:
+    if (left.colliderect(player2) or right.colliderect(player2)) and player.velocity_x == 0 and not feet_rect.colliderect(player2):# and player2.velocity_x != 0:
         adjust_bg = True
         check_tile_collision_y()
         if right.colliderect(player2):
             player.x = player2.x-player.width-1
         elif left.colliderect(player2):
             player.x = player2.x+player2.width+1
-    elif tile is not None and player.velocity_x != 0:
+    elif tile is not None and player.velocity_x != 0 and not (left.colliderect(player2) or right.colliderect(player2)):
         adjust_bg = True
         if player.velocity_x < 0 or player.direction == "left":
             player.x = tile.x+(TILE_SIZE if tile.x != player2.x else player2.width)
