2e3b3e48324fedacc1f80c92676db3dbfadbc27a
phantomized <boon.atticus@gmail.com>
2025-05-29 09:53:51 -0700

    setup fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ executables = [cx_Freeze.Executable("main.py")]
 
 cx_Freeze.setup(
     name="Celeste 2 client side",
-    options={"build_exe": {"packages":["pygame","os","json","random","subprocess","socket"],
+    options={"build_exe": {"packages":["pygame","os","json","random","subprocess","socket","network","pathlib","sys","_thread"],
                            "include_files":["Test Sprites","server.py","network.py"]}},
     executables = executables
 
