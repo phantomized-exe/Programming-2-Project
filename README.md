f9508700def10b9596fa7f0a662e0cbf550acf38
phantomized <boon.atticus@gmail.com>
2025-05-29 09:59:19 -0700

    setup additions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Programming-2-Project
 My final project for Computer Programming 2 at WLHS
+If you are running the .exe file, select main.exe and then procede as normal
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="Celeste 2 client side",
     options={"build_exe": {"packages":["pygame","os","json","random","subprocess","socket","network","pathlib","sys","_thread"],
-                           "include_files":["Test Sprites","server.py","network.py"]}},
+                           "include_files":["Test Sprites","server.py","network.py","Resources.txt","README.md"]}},
     executables = executables
 
     )
